342c98bad63cc43fb4a4d1fd120957b66af8b06a
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-06-26 11:55:37 -0400

    added Brazilian Real Conversion

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ function convertUSDtoWon(event) { // conversion
   // Now display on page:
   document.querySelector('h2').innerText = 
   "$" + usd.toLocaleString() + " USD is equal to " + won.toLocaleString() + " Korean Won"
+
+  const brl = usd * 4.77;
+  const brlH3 = document.createElement('h3');
+
+  brlH3.innerText = `$${usd.toLocaleString()} U.S. Dollar is equal to R$${brl.toLocaleString()} Brazilian Real`;
+  document.querySelector("body").appendChild(brlH3);
 }
