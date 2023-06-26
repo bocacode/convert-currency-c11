4ab99cddff82c246e0057cb811a24301e5c8680e
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-26 11:54:05 -0400

    Added Ruble Converesion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('form') // grab the form
 form.addEventListener("submit", convertUSDtoWon) // onsubmit
 
+const rubleH3 = document.createElement("h3");
+document.querySelector('body').appendChild(rubleH3)
+
 function convertUSDtoWon(event) { // conversion
   event.preventDefault() // stop form from refreshing page
   const usd = Number(event.target.usd.value) // get form value
@@ -8,4 +11,7 @@ function convertUSDtoWon(event) { // conversion
   // Now display on page:
   document.querySelector('h2').innerText = 
   "$" + usd.toLocaleString() + " USD is equal to " + won.toLocaleString() + " Korean Won"
+
+  const ruble = usd / 0.012
+  rubleH3.innerText = "$" + usd.toLocaleString() + " USD is equal to " + ruble.toLocaleString() + " Russian Ruble"
 }
