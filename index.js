69e00b005b845e231f6db86876b4b8b2f77280d9
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-06-26 12:26:14 -0400

    added egyptian pound conversion

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function convertUSDtoWon(event) { // conversion
   const won = usd / 0.00078 // convert to Won
   // Now display on page:
   document.querySelector('h2').innerText = 
-  "$" + usd.toLocaleString() + " USD is equal to " + won.toLocaleString() + " Korean Won"
+  "$" + usd.toLocaleString() + " USD is equal to " + won.toLocaleString() + " Egyptian Pounds"
 
   const egp = usd * 30.90 // convert to egyptian currency
   const egpH3 = document.createElement('h3')
